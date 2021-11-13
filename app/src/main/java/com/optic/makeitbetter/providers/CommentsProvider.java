a686572ea58677505d2ee01d2850c1d048be2526
Nicolas Benjamin Cruz Carpio <nicolasbncruz@hotmail.com>
2021-11-13 10:57:07 -0500

    feat: tipos de rutinas

---
+++ app/src/main/java/com/optic/makeitbetter/providers/CommentsProvider.java
@@ -0,0 +1,26 @@
+package com.optic.makeitbetter.providers;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.optic.makeitbetter.models.Comment;
+
+public class CommentsProvider {
+
+    CollectionReference mCollection;
+
+    public CommentsProvider() {
+        mCollection = FirebaseFirestore.getInstance().collection("Comments");
+    }
+
+    public Task<Void> create(Comment comment) {
+        return mCollection.document().set(comment);
+    }
+
+    public Query getCommentsByPost(String idPost) {
+        return mCollection.whereEqualTo("idPost", idPost);
+    }
+
+
+}
